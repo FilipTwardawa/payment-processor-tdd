afcb82640ac01f2c1ae53a79cc2b3d5c8357ec7e
FilipTwardawa <i15.twardawa@gmail.com>
2024-11-18 00:32:54 +0100

    Adding gradle files to the project to manage dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // Logging
+    implementation("org.slf4j:slf4j-api:2.0.9")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+application {
+    mainClass.set("org.example.payment.Main")
+}
