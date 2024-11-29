be71d5d8c45404813177e831fa5333f4b3a6cd2e
FilipTwardawa <i15.twardawa@gmail.com>
2024-11-29 11:08:01 +0100

    Adding changes to the build.gradle.kts file to support CI/CD

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("application")
+    id("jacoco")
 }
 
 group = "org.example"
@@ -13,7 +14,6 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-
 
     implementation("org.slf4j:slf4j-api:2.0.9")
     runtimeOnly("org.slf4j:slf4j-simple:2.0.9")
@@ -28,6 +28,18 @@ tasks.test {
     useJUnitPlatform()
     testLogging {
         events("PASSED", "FAILED", "SKIPPED")
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
     }
 }
 
